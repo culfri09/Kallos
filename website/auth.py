5207425bfe25ad46ba114e1d04c253aeee34baa1
Marta Lendínez <95087997+culfri09@users.noreply.github.com>
2024-04-18 11:59:47 +0200

    Do correct password checks

--- website/auth.py
+++ website/auth.py
@@ -4,6 +4,9 @@ from .import models
 from .import init
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, login_required, logout_user, current_user
+import re
+
+
 auth = Blueprint('auth', __name__)
 
 # Route for user login
@@ -58,10 +61,22 @@ def sign_up():
             flash('Email must be greater than 3 characters', category='error')
         elif len(first_name) < 2:
             flash('First name must be greater than 1 characters', category='error')
+        # Password checks_
+        # Passwords must match
         elif password1 != password2:
             flash('Passwords don\'t match', category='error')
+        # Password must be at least 7 characters. 
         elif len(password1) < 7:
             flash('Passwords must be at least 7 characters', category='error')
+        # Must contain at least 1 capital letter
+        elif not any(char.isupper() for char in password1):
+            flash('Password must contain at least 1 capital letter', category='error')
+        # Must contain at least 1 number
+        elif not any(char.isdigit() for char in password1):
+            flash('Password must contain at least 1 digit', category='error')
+        # Must contain at least 1 special character
+        elif not re.search(r'[!@#$%^&*()_+={}\[\]:;"\'|<,>.?/~`]$', password1):
+            flash('Password must contain at least 1 special character', category='error')
         else:
             # Creates account
             new_user = models.User(email=email, first_name=first_name, company_name = company_name, job_title = job_title, department = department, password=generate_password_hash(password1, method='pbkdf2:sha256'))
@@ -71,6 +86,5 @@ def sign_up():
             flash('Account created!', category='success')
             # Loads homepage
             return redirect(url_for('views.home'))
-
 
     return render_template("sign_up.html")
