4955c05ff5f39e249e6add530434be8323287f12
Marta Lendínez <95087997+culfri09@users.noreply.github.com>
2024-05-04 17:36:02 +0200

    implement iterations on encryption.py based on pylint

--- website/encryption.py
+++ website/encryption.py
@@ -1,18 +1,45 @@
+"""
+Module for encryption methods.
+"""
 import nacl.secret
 import nacl.utils
 from nacl.encoding import Base64Encoder
-# Encryption methods
 
 def encrypt(message, key):
+    """
+    Encrypts the given message using the provided key.
+
+    Args:
+        message (str): The message to encrypt.
+        key (bytes): The encryption key.
+
+    Returns:
+        str: The encrypted message.
+    """
     box = nacl.secret.SecretBox(key)
     encrypted = box.encrypt(message.encode(), encoder=Base64Encoder)
     return encrypted.decode('utf-8')
 
 def decrypt(encrypted_message, key):
+    """
+    Decrypts the given encrypted message using the provided key.
+
+    Args:
+        encrypted_message (str): The encrypted message.
+        key (bytes): The decryption key.
+
+    Returns:
+        str: The decrypted message.
+    """
     box = nacl.secret.SecretBox(key)
     decrypted = box.decrypt(encrypted_message.encode(), encoder=Base64Encoder)
     return decrypted.decode('utf-8')
 
 def generate_key():
-    return nacl.utils.random(nacl.secret.SecretBox.KEY_SIZE)
+    """
+    Generates a random encryption key.
 
+    Returns:
+        bytes: The generated encryption key.
+    """
+    return nacl.utils.random(nacl.secret.SecretBox.KEY_SIZE)
